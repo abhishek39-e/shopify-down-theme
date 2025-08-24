c0836ec83aac7ae8e3a88a22faa9efeff07fa58e
red arch <abhishekkaush39@gmail.com>
2025-08-24 21:04:31 +0530

    upd

--- assets/customCart-drawer.js
+++ assets/customCart-drawer.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     openCart.addEventListener('click', function (e) {
         e.preventDefault();
         cartDrawer.classList.add('show')
-        console.log('200 ok');
+        console.log('200 ');
     });
 
     let remove = document.querySelector('.cut-btn')
