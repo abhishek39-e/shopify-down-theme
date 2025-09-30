513b8c5e9eb659211bab86b532e93f54cd72150f
red arch <abhishekkaush39@gmail.com>
2025-09-30 20:52:32 +0530

    update

--- assets/cart.js
+++ assets/cart.js
@@ -12,7 +12,6 @@ class CartRemoveButton extends HTMLElement {
 customElements.define('cart-remove-button', CartRemoveButton);
 
 class CartItems extends HTMLElement {
-
   constructor() {
     super();
     this.lineItemStatusElement =
